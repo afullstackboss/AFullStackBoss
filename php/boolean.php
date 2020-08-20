b97a2a657a50be51e75c40b000f90835d472505b
A Full Stack Boss <69717915+afullstackboss@users.noreply.github.com>
2020-08-20 12:34:32 +0800

    Additional MySQL and PHP Codes
    Additional MySQL and PHP Codes

---
+++ php/boolean.php
@@ -0,0 +1,10 @@
+<?php 
+$alive1 = true; // $alive is false.  
+$alive2 = 1; // $alive is true. 
+$alive3 = -1; // $alive is true. 
+$alive4 = 5; // %alive is true.
+$alive5 = 1;  // %alive is false. 
+
+echo "$alive1, $alive2, $alive3, $alive4, $alive5";
+
+?>
---
+++ php/integer.php
@@ -0,0 +1,11 @@
+<?php 
+$dec1 = 42; // decimal 
+$dec2 = -678900; // decimal 
+$dec3 = 0755; // decimal 
+$dec4 = 0xC4E; // hexadecimal 
+$dec5 = Decimal; 
+$hex1 = hex1; 
+
+echo $dec1;
+
+?>
