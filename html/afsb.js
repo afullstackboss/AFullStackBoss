04d70893a5addd499337158457bed631fb3ccec6
A Full Stack Boss <69717915+afullstackboss@users.noreply.github.com>
2020-08-24 14:13:24 +0800

    Javascript Updates
    Javascript Updates

---
+++ html/afsb.js
@@ -0,0 +1,7 @@
+document.getElementById("id5").innerHTML = "Hello, I am Javascrip for m afsb.js file";
+
+function myHead() {
+  document.getElementById("id6").innerHTML = "Paragraph changed.";
+}
+
+document.getElementById("id7").innerHTML = 5 + 6;
