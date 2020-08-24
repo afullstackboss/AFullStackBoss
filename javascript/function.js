58e6d294e99d1815502440e2c6acd21b55cd50b8
A Full Stack Boss <69717915+afullstackboss@users.noreply.github.com>
2020-08-24 11:38:18 +0800

    Updates
    Updates

---
+++ javascript/function.js
@@ -0,0 +1,10 @@
+// Regular Function 
+function multiply(a,b) {
+	// code here
+	var results = 3 * 4;
+	console.log("3 muliplied by 4", results);
+	
+}
+
+multiply();
+document.getElementById("f1").innerHTML = myFunction(multiply);
--- javascript/index.js
+++ javascript/index.js
@@ -41,5 +41,91 @@
 //selectedColors[2] = '1';
 //console.log(selectedColors); 
 
-var date = new Date(); // Varible declaration 
-document.getElementById("id1").innerHTML = "<h1>Today is: " + date + "</h1>" //Alert JavaScript
+// Get Current Computer Time 
+//var date = new Date(); // Varible declaration 
+//document.getElementById("id1").innerHTML = "<h1>Today is: " + date + "</h1>" //Alert JavaScript
+//console.log(date); 
+// Addition 
+//document.getElementById("r1").innerHTML = 5 * 6 + 777 - 38 ; //5 + 6
+//console.log(); 
+// Multiplication 
+//document.getElementById("r2").innerHTML = 5 * 6; //5 + 6
+//console.log(); 
+// Multiplication 
+//document.getElementById("r3").innerHTML = 5 / 6; //5 + 6
+//console.log(); 
+// Get Reminder 
+//document.getElementById("r4").innerHTML = 5 % 6; //5 + 6
+//console.log(); 
+// Get Reminder 
+//document.getElementById("r5").innerHTML = 4 * 3 * 6 + 6; //5 + 6
+//console.log(); 
+// Get Reminder 
+//document.getElementById("r6").innerHTML = 55 * 35 * 45 - 34; //5 + 6
+//console.log(); 
+
+// Regular Function, call explicityl by name: 
+//function multiply(a,b) {
+	// code here
+	//var results = 3 * 4;
+	//console.log("2 multiplied by 4 is", results);
+//}
+//multiply();
+
+
+// Anonymous function stored in variable 
+// Invoked by calling the variable as a funtion 
+//var divided = function () {
+	// code here
+	//var results = 3 / 4;
+	//console.log("3 divided by 4 is", results);
+//}
+//divided();
+
+// Immidiately Invoked Function Experession.
+// Runs as soon as the browser finds it: 
+//(function () {
+	// code here
+	//var results = 12 / 0.75;
+	//console.log("12 divided by 0.75 is", results);
+//}())
+
+//function findBiggestFraction () {
+	//a>b ? console.log("a:", a) : console.log ("b:",b);
+//}
+
+
+//var firstFraction = 3/4; 
+//var secondFraction = 5/7;
+
+//findBiggestFraction(firstFraction, secondFraction);
+//findBiggestFraction(7/16,13/25);
+//findBiggestFraction(1/2,3/4);
+
+
+//document.getElementById("f1").innerHTML = findBiggestFraction();
+onclick="document.getElementById('f8').innerHTML = Date()"
+onclick='document.getElementById("f9").innerHTML = "Hello JavaScript!"'
+onclick="document.getElementById('myImage').src='pic_bulbon.gif'"
+onclick="document.getElementById('myImage').src='pic_bulboff.gif'"
+onclick="document.getElementById('f10').style.fontSize='35px'"
+onclick="document.getElementById('f11').style.display='true'"
+document.getElementById("f13").innerHTML = "My First JavaScript"
+
+
+// Another Function
+onclick="myText1()"
+function myText1() {
+  document.getElementById("f15").innerHTML = "Paragraph changed.";
+}
+
+
+
+
+
+
+
+
+
+
+
