470b359af9f8dc08cc8d015c2c6b7606e69b82a0
A Full Stack Boss <69717915+afullstackboss@users.noreply.github.com>
2020-08-24 08:20:22 +0800

    Updates
    Updates

---
+++ javascript/index.js
@@ -0,0 +1,45 @@
+//var date = new Date(); // Varible declaration 
+//document.getElementById("demo1").innerHTML = "<h1>Today is: " + date + "</h1>" /* Long Variable Comments */
+ //var a; 
+ //var b;
+ //var sum; 
+ //a = 5; 
+ //b = 4;
+ //sum = a + b; 
+//document.getElementById("demo").innerHTML = sum;
+
+//let name = 'Roland ';
+//console.log(name);
+
+//const interestRate = 0.3;
+//console.log('interestRate');
+
+//let name = 'Roland'; // String Literal 
+//let age = 30; // Number Literal 
+//let isApproved = false; // Boolean Literal 
+//let firtnName = undefined; 
+//let selectedColor = null; 
+//console.log('name');
+//console.log('age');
+//console.log('isApproved');
+//console.log('selectedColor');
+
+//let person = {
+	//name: 'Roland',
+	//age: 39 
+//};
+
+// Dot Notation  use this style 
+//person.name = 'name';
+
+// Bracket Notation 
+//let selection = 'name';
+//person [selection] = 'Mary';
+// console.log(person.name);
+
+//let selectedColors = ['red','blue'];
+//selectedColors[2] = '1';
+//console.log(selectedColors); 
+
+var date = new Date(); // Varible declaration 
+document.getElementById("id1").innerHTML = "<h1>Today is: " + date + "</h1>" //Alert JavaScript
